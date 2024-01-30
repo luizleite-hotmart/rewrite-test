509e12b4f97b3236cdaa8b6c28bfe8870ced7bf8
luiz.leite <luiz.leite@hotmart.com>
2024-01-30 08:24:05 -0300

    add empty project

---
+++ src/main/java/com/luizleiteoliveira/testrewriteevolutioninactions/TestRewriteEvolutionInActionsApplication.java
@@ -0,0 +1,13 @@
+package com.luizleiteoliveira.testrewriteevolutioninactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestRewriteEvolutionInActionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestRewriteEvolutionInActionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luizleiteoliveira/testrewriteevolutioninactions/TestRewriteEvolutionInActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizleiteoliveira.testrewriteevolutioninactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestRewriteEvolutionInActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
